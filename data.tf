19ddc0d5a14a93e28ff33431008d7957267d4957
naresh <Naresh@gmail.com>
2025-04-19 16:09:56 +0530

    tools automation

---
+++ data.tf
@@ -0,0 +1,15 @@
+data "aws_ami" "main" {
+ 
+  most_recent      = true
+  owners           = ["355449129696"]
+  
+ filter {
+    name   = "description"
+    values = ["DevOps-LabImage-RHEL9"]
+  }
+
+}
+
+data "aws_route53_zone" "main" {
+  name       = var.domain
+}
