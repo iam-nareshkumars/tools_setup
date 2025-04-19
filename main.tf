56368bcd8b0c59f4af76da51d28cb1cc7a6dc3a0
naresh <Naresh@gmail.com>
2025-04-19 21:14:07 +0530

    jenkins automation

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "null_resource" "main" {
     command = <<EOT
       sleep 10; 
       cd /home/ec2-user/tools_setup; 
-      ansible-playbook -i ${toolname}.clouding-app.shop, -e ansible_username=ec2-user -e ansible_password=DevOps321 -e toolname=${each.value["Name"]} roles\tool.yml
+      ansible-playbook -i inv -e ansible_username=ec2-user -e ansible_password=DevOps321 -e toolname=${each.value["Name"]} roles\tool.yml
     EOT
 
   }
