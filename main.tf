d34d669e6000e87194940eb7552db775a3f1ec2e
naresh <Naresh@gmail.com>
2025-04-21 10:35:35 +0530

    jenkins automation

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ module "tools" {
 
   source        = "./modules"
   ami           = data.aws_ami.main.id
-  instance_type = each.value["instance_type"]
-  Name          = each.value["Name"]
+  instance_type = var.tools["vault"].instance_type
+  Name          = var.tools["vault"].Name
   zone_id       = data.aws_route53_zone.main.zone_id
   domain        = var.domain
-  port_no       = each.value["port_no"]
+  port_no       = var.tools["vault"].port_no
   tools         = var.tools
 }
 
