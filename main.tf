329d71620e16d0c6e6a61180dd2312ffeeb10709
naresh <Naresh@gmail.com>
2025-04-21 10:32:33 +0530

    jenkins automation

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "tools" {
-  for_each      = var.tools
+
   source        = "./modules"
   ami           = data.aws_ami.main.id
   instance_type = each.value["instance_type"]
@@ -7,5 +7,6 @@ module "tools" {
   zone_id       = data.aws_route53_zone.main.zone_id
   domain        = var.domain
   port_no       = each.value["port_no"]
+  tools         = var.tools
 }
 
