edb640f3e17e6305670699f2648d8e52c7841f40
naresh <Naresh@gmail.com>
2025-04-21 14:25:07 +0530

    jenkins automation

--- variables.tf
+++ variables.tf
@@ -8,11 +8,7 @@ variable "tools" {
       instance_type = "t3.small"
       port_no       = "8200"
     }
-    elk = {
-      Name          = "elk"
-      instance_type = "t3.small"
-      port_no       = "53"
-    }
+   
   }
 
 }
