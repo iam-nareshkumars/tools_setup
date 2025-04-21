a1e26608a3f4befe16765825d595788a4ec15344
naresh <Naresh@gmail.com>
2025-04-21 18:27:12 +0530

    jenkins automation

--- modules/main.tf
+++ modules/main.tf
@@ -80,7 +80,7 @@ resource "null_resource" "main" {
     inline = [
       "sleep 10",
       "pwd",
-      "ansible-playbook -i ${var.Name}.eternallearnings.shop  -e toolname=${var.Name} tools_setup/tools.yml"
+      "ansible-playbook -i ${var.Name}.eternallearnings.shop  -e toolname=${var.Name} /home/ec2-user/tools_setup/tools.yml"
     
     ]
   }
