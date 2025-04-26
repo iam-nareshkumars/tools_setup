30a79679205cd060d246e41c9f85382b8b9b0835
naresh <Naresh@gmail.com>
2025-04-26 13:44:16 +0530

    jenkins automation

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 module "tools" {
   for_each      = var.tools
-  source        = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
+  source        = "./modules"
   ami           = data.aws_ami.main.id
   instance_type = each.value["instance_type"]
   Name          = each.value["Name"]
   zone_id       = data.aws_route53_zone.main.zone_id
   domain        = var.domain
   port_no       = each.value["port_no"]
-  env           = "prod"
+
 
 }
 
