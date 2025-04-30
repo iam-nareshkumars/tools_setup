0dd60e000b037876f1e4fd25633f50db614cfd96
naresh <Naresh@gmail.com>
2025-04-30 19:14:10 +0530

    jenkins automation

--- modules/main.tf
+++ modules/main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "main" {
   ami                    = data.aws_ami.main.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
-  iam_instance_profile   = aws_iam_instance_profile.main.arn
+  iam_instance_profile   = aws_iam_instance_profile.main.name
 
   tags = {
     Name = "${var.Name}-tool"
