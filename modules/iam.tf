661102680542457fffcc7f1e718b8c9e9dd9610d
naresh <Naresh@gmail.com>
2025-04-30 18:32:07 +0530

    jenkins automation

--- modules/iam.tf
+++ modules/iam.tf
@@ -46,6 +46,6 @@ resource "aws_iam_role_policy" "main" {
 }
 
 resource "aws_iam_instance_profile" "main" {
-  name = "iam_instance_profile"
+  name = "${var.Name}-iam_instance_profile"
   role = aws_iam_role.main.id
 }
