7a9830f29a1fc22830094257861a99f4a19d0e2a
naresh <Naresh@gmail.com>
2025-04-30 17:18:46 +0530

    jenkins automation

---
+++ modules/iam.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_role" "main" {
+  name = "${var.Name}-IAM-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${var.Name}-IAM-role"
+  }
+}
+
+
+resource "aws_iam_role_policy" "main" {
+  name = "${var.Name}-IAM-role-policy"
+  role = aws_iam_role.main.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:DescribeAvailabilityZones"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
