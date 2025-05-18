585f2e90fd0d8e3b25261937b6d9f45982d9a88a
naresh <Naresh@gmail.com>
2025-05-18 20:49:54 +0530

    kubernetes

--- modules/data.tf
+++ modules/data.tf
@@ -1,11 +1,11 @@
 data "aws_ami" "main" {
 
   most_recent = true
-  owners      = ["703671922956"]
+  owners      = ["355449129696"]
 
   filter {
     name   = "description"
-    values = ["MyPracticeImage"]
+    values = ["DevOps-LabImage-RHEL9"]
   }
 
 }
